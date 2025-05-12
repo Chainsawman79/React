2deb26511a167ec3d2e85944c4299093c489685d
Charchit73 <148005866+Chainsawman79@users.noreply.github.com>
2025-05-12 16:01:26 +0530

    Add files via upload

---
+++ react.js
@@ -0,0 +1,22 @@
+//React is a library ,not a framework
+//react uses react virtual DOM(Document Object Model)
+//react first creates an exact copy of DOM
+//react figures out which part is new and only specific part in the virtual DOM
+//react copies only the new 
+//JSX(Javascript XML) is a syntax extension to javascript ,by Babel
+// 1.We can't return more than one HTML element at once,but we can wrap the elements inside a parent HTML tag
+//2.We can use JSX inside for loops,if else clauses
+// Functional Components(stateless) is basically a javascript 
+// function Welcome(props){
+//     return <h1>Hello,[props.name]</h1>;
+// } 
+
+// function Example(){
+//     return(<h1>I'm a Functional Component !</h1>);
+// }
+// const Example=() =>{
+//     return(<h1>I'm a functional component!</h1>);
+// };
+
+// Class(Stateful) components
+//
