93485d3429d8dfe61d920415e5ba3b1c494b91e6
Charchit73 <148005866+Chainsawman79@users.noreply.github.com>
2025-05-12 16:27:19 +0530

    Add files via upload

--- react.js
+++ react.js
@@ -11,6 +11,8 @@
 //     return <h1>Hello,[props.name]</h1>;
 // } 
 
+//const { Component } = require("react");
+
 // function Example(){
 //     return(<h1>I'm a Functional Component !</h1>);
 // }
@@ -19,4 +21,28 @@
 // };
 
 // Class(Stateful) components
-//
+//Props : React is a component-based library which divides the UI into little resusable peices.
+// What is "Props" and how to use it in React?
+//  * React has a different approach to data flow & manipulation than other frameworks.
+// What is Props?
+//  * React is a component-based library which divides the UI into little reusable pieces. In some cases, those components need to communicate (send data to each other) and the way to pass data between components is by using props.
+//  * "Props" is a special keyword in React, which stands for properties and is being used for passing data from one component to another.
+//  * But the important part here is that data with props are being passed in a uni-directional flow. (one way from parent to child)
+//  * Furthermore, props data is read-only, which means that data coming from the parent should not be changed by child components.
+// class ParentComponent extends Component{
+//     render(){
+//         return(
+//             <h1>
+//             I'm the parent component.
+//             <ChildComponent />
+//             </h1>
+//         );
+//{/* <ChildComponent someAttribute={value}
+// const addition (firstNum,secondNum)=>(
+//     return firstNum+secondNum;
+// ); */}
+
+// const ChildCompont=(props)=>(
+//     return <p>{props.text}</p>
+// );
+
